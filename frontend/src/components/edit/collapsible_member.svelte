0625a59e95f18db1ce67532735ea34b6831e68c2
Tyler Moore <TylerRMoore@gmail.com>
2024-02-07 17:17:55 -0800

    Fixed min heights, collapsible members

---
+++ frontend/src/components/edit/collapsible_member.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import IconButton, { Icon } from '@smui/icon-button';
+	
+    export let name:string;
+    let show_children=false;
+</script>
+
+<div>
+    <div class="top">
+        <IconButton on:click={() => {}} toggle bind:pressed={show_children}>
+            <Icon class="material-icons" on>arrow_drop_down</Icon>
+            <Icon class="material-icons">arrow_right</Icon>
+        </IconButton>
+        {name}
+    </div>
+    {#if show_children}
+        <slot name="contents"/>
+    {/if}
+</div>
+
+<style>    
+    .top {
+        display:flex;
+        flex-direction: row;
+        justify-content: left;
+        align-content: center;
+    }
+</style>
--- frontend/src/components/edit/manifest_edit_component.svelte
+++ frontend/src/components/edit/manifest_edit_component.svelte
@@ -2,16 +2,59 @@
 	import type { RotationManifest } from "../manifest/RotationManifest";
 	import Textfield from '@smui/textfield';
 	import RotationEditComponent from "./rotation_edit_component.svelte";
+    import CollapsibleMember from "./collapsible_member.svelte";
 	export let manifest:RotationManifest;
 </script>
 
-<div>
-    <Textfield bind:value={manifest.title} on:change={()=>console.debug(manifest)} label="Manifest Title" />
-    {#each manifest.rotation_manifest as rotation}
-        <RotationEditComponent bind:rotation={rotation}/>
-    {/each}
+<div class="outer">
+    <div>
+        <Textfield bind:value={manifest.title} on:change={()=>console.debug(manifest)} label="Manifest Title" />
+    </div>
+    <div class="container middle">
+        <CollapsibleMember name="Rotations">
+            <div slot="contents" class="container inner">
+                {#each manifest.rotation_manifest as rotation}
+                    <div class="container">
+                        <RotationEditComponent bind:rotation={rotation}/>
+                    </div>
+                {/each}
+            </div>
+        </CollapsibleMember>
+    </div>
 </div>
 
-<style>
+<style>    
+    .container {
+        border-width: 1px;
+        border-style: solid;
+        border-radius: 10px;
+        margin: 3px;
+        padding: 10px;
+        flex-shrink: 1;
+    }
 
+    .outer {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+        height: 100%;
+        max-height: 100%;
+        margin-left: 5px;
+    }
+
+    .middle {
+        display: flex;
+        flex-direction: column;
+        flex-shrink: 1;
+        flex-grow: 1;
+        min-height: 0px;
+    }
+
+    .inner {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+        height: 100%;
+        overflow:auto;
+    }
 </style>
