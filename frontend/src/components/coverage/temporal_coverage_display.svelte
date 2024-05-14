5c9aff538b4f251261c75ae92054915905334c4a
Tyler Moore <TylerRMoore@gmail.com>
2024-05-13 18:56:19 -0700

    Correct coverage query display

--- frontend/src/components/coverage/temporal_coverage_display.svelte
+++ frontend/src/components/coverage/temporal_coverage_display.svelte
@@ -6,11 +6,20 @@
 	export let day:number;
 
 	$:dowstring=shortdowfunc(day)+" ";
+
+	let dowrotation:number=0;
+	$:{
+		dowrotation=day+coverage.work_to_rotation_day_offset;
+		while(dowrotation>6)
+		{
+			dowrotation-=7;
+		}
+	}
 </script>
 
 <tr>
     <td>{coverage.rotation}</td>
-	<td>{dowfunc(coverage.work_to_rotation_day_offset).toString()}</td>
+	<td>{dowfunc(dowrotation).toString()}</td>
 	<td>{dowstring+minutes_since_midnight_to_time_string(coverage.start)}</td>
 	<td>{dowstring+minutes_since_midnight_to_time_string(coverage.end)}</td>
 </tr>
