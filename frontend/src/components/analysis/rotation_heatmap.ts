f065ab889205006f04e5af5b654d397cb3816b5f
Tyler Moore <TylerRMoore@gmail.com>
2024-05-21 12:33:24 -0700

    Chart improvements

--- frontend/src/components/analysis/rotation_heatmap.ts
+++ frontend/src/components/analysis/rotation_heatmap.ts
@@ -1,6 +1,7 @@
 import * as Plot from "@observablehq/plot";
 import type { AnalysisMark, Rotation_Analysis_Data } from "../../commons/rotation_plot_data";
 import type { ValueType } from "./commons";
+import { short_days_of_the_week_Mon_first, short_days_of_the_week_Sun_first } from "../../commons/time";
 
 export const get_rotation_marks=(data:Rotation_Analysis_Data)=>{
     let all_marks:AnalysisMark[]=[];
@@ -36,37 +37,80 @@ export const build_heatmap=(analysis_marks:AnalysisMark[], width:number, options
 
     let filtered_marks:AnalysisMark[]=analysis_marks.filter((mark)=>options.rotations.includes(mark.rotation));
 
+    const binYrect=Plot.binY(
+        {
+            fill:"proportion-facet",
+            z:"proportion-facet",
+        },
+        {
+            y: (d)=>d.value[options.valuetype],
+            fx: "rotation",
+            inset: 0,
+            channels: {
+                dates:{
+                    value: (d:AnalysisMark[])=>{
+                        const dates:Set<Date> = new Set();
+                        for(const member of d)
+                        {
+                            dates.add(member.date);
+                        }
+                        return dates;
+                    },
+                    label: "Dates"
+                }
+            },
+            sort: {
+                fx: "data",
+                reduce: (data:AnalysisMark[][]) => {
+                    let sum=0;
+                    let count=0;
+                    for(const mark_group of data)
+                    {
+                        for(const mark of mark_group)
+                        {
+                            sum+=mark.value[options.valuetype]; 
+                        }
+                        count+=mark_group.length;
+                    }
+                    return sum/count;
+                }
+            },
+            tip: {
+                format:{
+                    fill: (d,i)=>{return Math.trunc(d*1000)/10+"%";},
+                    fx: true,
+                    y: true,
+                    dates: (d:Set<Date>,i)=>{
+                        let retval="";
+                        for(const date of d)
+                        {
+                            let cust_date_str=short_days_of_the_week_Mon_first[date.getDay()]+" "+(date.getMonth()+1).toString()+"/"+(date.getDate()+1).toString()+"/"+date.getFullYear().toString().substring(2,4);
+                            retval+=cust_date_str+", ";
+                        }
+                        retval=retval.substring(0,retval.length-2);
+                        console.debug("Dates: " + retval);
+                        return retval;
+                    },
+                },
+                dates:(d:any)=>{console.debug("dates",d);}
+            } as Plot.TipOptions
+        } as Plot.BinYInputs<Plot.RectOptions>
+    );
+
+
     let marks:any=[
         Plot.rect(
             filtered_marks,
-            Plot.binY(
-                {
-                    fill:"proportion-facet",
-                    z:"proportion-facet"
-                },
-                {
-                    y: (d)=>d.value[options.valuetype],
-                    fx: "rotation",
-                    inset: 0,
-                    sort: {
-                        fx: "data",
-                        reduce: (data:AnalysisMark[][]) => {
-                            let sum=0;
-                            let count=0;
-                            for(const mark_group of data)
-                            {
-                                for(const mark of mark_group)
-                                { 
-                                    sum+=mark.value[options.valuetype]; 
-                                }
-                                count+=mark_group.length;
-                            }
-                            return sum/count;
-                        }
-                    }
-                } as Plot.BinYInputs<Plot.RectOptions>
+            binYrect
+        ),
+        /*
+        Plot.tip(
+            filtered_marks,
+            Plot.pointer(
+                binYpoint
             )
         ),
+        */
         /*
         Plot.tip(
             displayed_marks,
@@ -103,6 +147,8 @@ export const build_heatmap=(analysis_marks:AnalysisMark[], width:number, options
         fx:{
             tickRotate:-45,
         },
+        fy:{
+        } as Plot.ScaleOptions,
         marks: marks,
         style:{
             fontSize:"14px"
